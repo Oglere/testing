8771e0af7e4d8b1db9c035feadb9dfaaabd07e32
Gayle <davigaru0@gmail.com>
2024-06-27 23:15:01 -0700

    Gayle | Jun 27

---
+++ PHP Folder/Index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS Folder/index.scss">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
+
+<script>
+    const dolls = {
+        "Doll1": {
+            "hair": {
+                "hair1": "blue",
+                "hair2": "red",
+                "hair3": "purple"
+            },
+            "skin": "tanned"
+        },
+        "Doll2": {
+            "hair": "red",
+            "skin": "white"
+        }
+    };
+
+    console.log(dolls.Doll1);
+</script>
